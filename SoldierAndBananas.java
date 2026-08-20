2edbf83ad63288cdb9f3e5e39fdec7e0dba169c3
Pratham Srivastava <prathamsri04@gmail.com>
2026-08-20 16:33:44 +0530

    Rename Java file

--- SoldierAndBananas.java
+++ SoldierAndBananas.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SoldieAndBananas {
+public class SoldierAndBananas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
